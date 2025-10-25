d60815193fcdaa8e778f96da01f6e307cccc03c1
Ion Utale <utale.ion@gmail.com>
2025-10-25 23:33:21 +0200

    refactor: wrap user creation script in an IIFE and use environment variable for database name

--- docker/mongo-init/01-create-local-user.js
+++ docker/mongo-init/01-create-local-user.js
@@ -1,19 +1,21 @@
 // This script runs only on first container init (empty data dir)
 // It creates the application user for the sv-app database.
 
-try {
-  const dbName = process.env.MONGO_INITDB_DATABASE || 'sv-app';
-  const user = 'local-user';
-  const pwd = '1234567890';
+(function () {
+  try {
+    var dbName = 'sv-app';
+    var user = 'local-user';
+    var pwd = '1234567890';
 
-  const db = db.getSiblingDB(dbName);
-  db.createUser({
-    user,
-    pwd,
-    roles: [{ role: 'readWrite', db: dbName }]
-  });
+    var appdb = db.getSiblingDB(dbName);
+    appdb.createUser({
+      user: user,
+      pwd: pwd,
+      roles: [{ role: 'readWrite', db: dbName }]
+    });
 
-  print(`Created user ${user} with readWrite on ${dbName}`);
-} catch (e) {
-  print(`User creation script error: ${e}`);
-}
+    print('Created user ' + user + ' with readWrite on ' + dbName);
+  } catch (e) {
+    print('User creation script error: ' + e);
+  }
+})();
