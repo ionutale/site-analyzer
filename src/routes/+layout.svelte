b0612fd260a3223f775c4bb2d71cb0c2af9a5acf
Ion Utale <utale.ion@gmail.com>
2025-10-26 06:44:50 +0100

    refactor(layout): update drawer behavior and simplify header structure

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -82,12 +82,12 @@
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
-<div class="drawer min-h-screen">
+<div class="drawer min-h-screen xl:drawer-open">
 	<input id="drawer-toggle" type="checkbox" class="drawer-toggle" />
 	<div class="drawer-content flex flex-col">
 		<div class="navbar bg-base-200">
 			<div class="container mx-auto px-4">
-				<div class="flex-none lg:hidden">
+				<div class="flex-none xl:hidden">
 					<label for="drawer-toggle" class="btn btn-square btn-ghost">
 						<svg
 							xmlns="http://www.w3.org/2000/svg"
@@ -106,66 +106,7 @@
 				<div class="flex-1">
 					<a href={resolve('/')} class="btn text-xl btn-ghost">Site Analyzer</a>
 				</div>
-				<div class="hidden flex-none items-center gap-2 lg:flex">
-					<ul class="menu menu-horizontal px-1">
-						<li><a href={resolve('/')}>Home</a></li>
-						<li><a href={resolve('/analyzer')}>Analyzer</a></li>
-						<li><a href={resolve('/sites')}>Sites</a></li>
-					</ul>
-					{#if $user}
-						<div class="dropdown dropdown-end">
-							<div tabindex="0" role="button" class="btn btn-ghost gap-2">
-								{#if $user.photoURL}
-									<img src={$user.photoURL} alt="Avatar" class="mask mask-squircle h-6 w-6" />
-								{/if}
-								<span class="hidden md:inline">{$user.displayName || 'Account'}</span>
-							</div>
-							<ul class="menu dropdown-content z-10 w-52 rounded-box bg-base-200 p-2 shadow">
-								<li><a href={resolve('/profile')}>Profile</a></li>
-								<li><button onclick={logout}>Logout</button></li>
-							</ul>
-						</div>
-					{:else}
-						<a class="btn" href={resolve('/login')}>Login</a>
-					{/if}
-					<div class="tooltip" data-tip={themeLabel()}>
-						<button
-							class="btn btn-square btn-ghost"
-							aria-label={themeLabel()}
-							onclick={cycleThemePref}
-						>
-							{#if themePref === 'light'}
-								<svg
-									xmlns="http://www.w3.org/2000/svg"
-									viewBox="0 0 24 24"
-									fill="currentColor"
-									class="h-5 w-5"
-									><path
-										d="M12 18a6 6 0 1 0 0-12 6 6 0 0 0 0 12Zm0 4a1 1 0 0 1 1 1v1h-2v-1a1 1 0 0 1 1-1Zm0-22a1 1 0 0 1 1-1V0h-2v0a1 1 0 0 1 1-1ZM0 13a1 1 0 0 1-1-1H0v2H0a1 1 0 0 1-1-1Zm24 0a1 1 0 0 1-1 1v0h-2v-2h2v0a1 1 0 0 1 1 1ZM4.222 20.364a1 1 0 0 1 0-1.414l.707-.707 1.414 1.414-.707.707a1 1 0 0 1-1.414 0ZM17.657 6.929l.707-.707a1 1 0 0 1 1.414 1.414l-.707.707-1.414-1.414ZM3.515 5.515a1 1 0 0 1 1.414-1.414l.707.707-1.414 1.414-.707-.707Zm14.142 14.142.707.707a1 1 0 1 1-1.414 1.414l-.707-.707 1.414-1.414Z"
-									/></svg
-								>
-							{:else if themePref === 'dark'}
-								<svg
-									xmlns="http://www.w3.org/2000/svg"
-									viewBox="0 0 24 24"
-									fill="currentColor"
-									class="h-5 w-5"
-									><path d="M21.64 13.64A9 9 0 1 1 10.36 2.36a7 7 0 1 0 11.28 11.28Z" /></svg
-								>
-							{:else}
-								<svg
-									xmlns="http://www.w3.org/2000/svg"
-									viewBox="0 0 24 24"
-									fill="currentColor"
-									class="h-5 w-5"
-									><path
-										d="M6 2a1 1 0 0 1 1 1v1h10V3a1 1 0 1 1 2 0v1h1a2 2 0 0 1 2 2v2H3V6a2 2 0 0 1 2-2h1V3a1 1 0 0 1 1-1ZM3 10h18v8a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-8Z"
-									/></svg
-								>
-							{/if}
-						</button>
-					</div>
-				</div>
+				<!-- header right side intentionally minimal; auth and theme live in the drawer -->
 			</div>
 		</div>
 		<main class="container mx-auto flex-1 px-4 py-6">
